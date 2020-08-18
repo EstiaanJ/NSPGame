03c0b08afc0ff0daef4798c94ba9e39e19ca47c3
estiaan <estiaan.jrensburg@gmail.com>
2020-08-18 19:34:09 +0800

    initial commit

---
+++ src/Launcher.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class Launcher extends PApplet{
+    public static void main(String[] args){
+        Launcher.main("Launcher");
+    }
+
+    public void settings(){
+        size(900,900);
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+        background(0);
+
+    }
+}
